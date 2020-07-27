609342ab6cbbba29db6870aa0dfdd7d13a2e3c20
waiting7777 <waiting0819@msn.com>
2020-07-27 11:32:50 +0800

    modify cronJob time

--- index.js
+++ index.js
@@ -9,63 +9,63 @@ const dayjs = require('dayjs')
 const utils = require('./utils')
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 })
 
 connection.connect()
 
 const config = {
-    channelAccessToken: process.env.CHANNELACCESSTOKEN,
-    channelSecret: process.env.CHANNELSECRET
+  channelAccessToken: process.env.CHANNELACCESSTOKEN,
+  channelSecret: process.env.CHANNELSECRET
 }
 
 const client = new line.Client(config)
 
 function doGet(url) {
-    return new Promise((resolve, reject) => {
-        axios({
-            method: 'get',
-            url: `${url}`
-        })
-        .then(res => {
-            resolve(res.data)
-        })
-        .catch(error => {
-            resolve(error.response.data)
-        })
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'get',
+      url: `${url}`
     })
+    .then(res => {
+      resolve(res.data)
+    })
+    .catch(error => {
+      resolve(error.response.data)
+    })
+  })
 }
 
 function queryDB(sql) {
-    return new Promise((resolve, reject) => {
-        connection.query(sql, (err, rows) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(rows);
-            }
-        })
+  return new Promise((resolve, reject) => {
+    connection.query(sql, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
     })
+  })
 }
 
 async function getDailyPrice() {
-    const resM = await doGet('https://api.scryfall.com/cards/search?q=set:m21+rarity:m')
-    const dataM = resM.data.filter(v => v.collector_number < 274)
-    dataM.forEach(d => {
-        const queryString = `INSERT INTO daily_price (card_name, rarity, price) values (\"${d.name}\", "Mythic",  \"${d.prices.usd}\")`
-        queryDB(queryString)
-        console.log(queryString)
-    })
-    const resR = await doGet('https://api.scryfall.com/cards/search?q=set:m21+rarity:r')
-    const dataR = resR.data.filter(v => v.collector_number < 274)
-    dataR.forEach(d => {
-        const queryString = `INSERT INTO daily_price (card_name, rarity, price) values (\"${d.name}\", "Rare", \"${d.prices.usd}\")`
-        queryDB(queryString)
-        console.log(queryString)
-    })
+  const resM = await doGet('https://api.scryfall.com/cards/search?q=set:m21+rarity:m')
+  const dataM = resM.data.filter(v => v.collector_number < 274)
+  dataM.forEach(d => {
+    const queryString = `INSERT INTO daily_price (card_name, rarity, price) values (\"${d.name}\", "Mythic",  \"${d.prices.usd}\")`
+    queryDB(queryString)
+    console.log(queryString)
+  })
+  const resR = await doGet('https://api.scryfall.com/cards/search?q=set:m21+rarity:r')
+  const dataR = resR.data.filter(v => v.collector_number < 274)
+  dataR.forEach(d => {
+    const queryString = `INSERT INTO daily_price (card_name, rarity, price) values (\"${d.name}\", "Rare", \"${d.prices.usd}\")`
+    queryDB(queryString)
+    console.log(queryString)
+  })
 }
 
 async function getPrice(rarity, today, yesterday) {
@@ -75,26 +75,26 @@ async function getPrice(rarity, today, yesterday) {
     const resYesterday = await queryDB(queryString)
     const contents = []
     res.forEach((v, i) => {
-        const diff = +(Number(v.price) - Number(resYesterday[i].price)).toFixed(1)
-	const price = (v.price) ? Number(v.price).toFixed(1) : 'NULL'
-        contents.push({
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-              {
-                "type": "text",
-                "text": `${v.card_name}`,
-                "flex": 3
-              },
-              {
-                "type": "text",
-                "text": `$${price}${utils.stringNumber(diff)}`,
-                "align": "end",
-                "flex": 2,
-                "color": utils.stringColor(diff)
-              }
-            ]
-        })
+      const diff = +(Number(v.price) - Number(resYesterday[i].price)).toFixed(1)
+	    const price = (v.price) ? Number(v.price).toFixed(1) : 'NULL'
+      contents.push({
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "text",
+            "text": `${v.card_name}`,
+            "flex": 3
+          },
+          {
+            "type": "text",
+            "text": `$${price}${utils.stringNumber(diff)}`,
+            "align": "end",
+            "flex": 2,
+            "color": utils.stringColor(diff)
+          }
+        ]
+      })
     })
     return contents
 }
@@ -106,57 +106,57 @@ async function pushDailyPrice() {
     const rareContent = await getPrice('Rare', today, yesterday)
     
     const replyJSON = {
-        "type": "carousel",
-        "contents": [
-          {
-            "type": "bubble",
-            "header": {
-              "type": "box",
-              "layout": "horizontal",
-              "contents": [
-                {
+      "type": "carousel",
+      "contents": [
+        {
+          "type": "bubble",
+          "header": {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "M21-Mythic",
+                "weight": "bold"
+              },
+              {
                   "type": "text",
-                  "text": "M21-Mythic",
-                  "weight": "bold"
-                },
-                {
-                    "type": "text",
-                    "text": `${today}`,
-                    "align": "end"
-                }
-              ]
-            },
-            "body": {
-              "type": "box",
-              "layout": "vertical",
-              "contents": mythicContent
-            }
+                  "text": `${today}`,
+                  "align": "end"
+              }
+            ]
           },
-          {
-            "type": "bubble",
-            "header": {
-              "type": "box",
-              "layout": "horizontal",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "M21-Rare",
-                  "weight": "bold"
-                },
-                {
-                    "type": "text",
-                    "text": `${today}`,
-                    "align": "end"
-                }
-              ]
-            },
-            "body": {
-              "type": "box",
-              "layout": "vertical",
-              "contents": rareContent
-            }
+          "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": mythicContent
           }
-        ]
+        },
+        {
+          "type": "bubble",
+          "header": {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "M21-Rare",
+                "weight": "bold"
+              },
+              {
+                  "type": "text",
+                  "text": `${today}`,
+                  "align": "end"
+              }
+            ]
+          },
+          "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": rareContent
+          }
+        }
+      ]
     }
     client.pushMessage(process.env.GROUPID, {
         type: 'flex',
@@ -190,15 +190,15 @@ async function getDeck(type) {
   })
 }
 
-const getPriceJob = new CronJob('00 00 08 * * *', () => {
+const getPriceJob = new CronJob('00 00 00 * * *', () => {
   getDailyPrice()
 })
 
-const pushPriceJob = new CronJob('00 01 08 * * *', () => {
+const pushPriceJob = new CronJob('00 00 08 * * *', () => {
   pushDailyPrice()
 })
 
-const getMetaJob = new CronJob('00 30 08 * * *', () => {
+const getMetaJob = new CronJob('00 01 00 * * *', () => {
   getDeck('Standard')
   setTimeout(() => {
     getDeck('Historic')
